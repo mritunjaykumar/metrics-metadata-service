34d108813a364fdfaec1870e916223cb6c1f3a9d
Mritunjay Kumar <mritunjay.kumar@rackspace.com>
2018-04-28 12:47:40 -0700

    first commit

---
+++ src/main/java/com/rackspacecloud/blueflood/metricsmetadataservice/MetricsMetadataServiceApplication.java
@@ -0,0 +1,12 @@
+package com.rackspacecloud.blueflood.metricsmetadataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsMetadataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricsMetadataServiceApplication.class, args);
+	}
+}
